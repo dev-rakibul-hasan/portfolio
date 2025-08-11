671d45747fbc46e8c3f9c174cfcc5234a57831f6
dev-rakibul-hasan <rsrakib1808@gmail.com>
2025-08-11 23:38:28 +0600

    Fix ALLOWED_HOSTS: Change .render.com to .onrender.com for Render deployment

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-5wp1*svac6^gtp_f2-=q1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.render.com', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition - Include apps conditionally
 INSTALLED_APPS = [
